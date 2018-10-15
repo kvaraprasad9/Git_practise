b4e2dba71c0df60851c9a05661f3b175fe61d1d8
varaprasad <vara.k.prasadzz@gmail.com>
2018-10-15 19:54:32 +0530

    first file

---
+++ src/one/Test_1.java
@@ -0,0 +1,16 @@
+package one;
+
+public class Test_1 {
+
+	public Test_1(){
+		System.out.println("this is demo");
+	}
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Test_1 t1=new Test_1();
+		
+		
+
+	}
+
+}
